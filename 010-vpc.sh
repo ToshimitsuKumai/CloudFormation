3cad9322271e34b05f416b04462d9b70b12092dd
熊井利充 <the.brown.bunnyy@gmail.com>
2020-05-09 17:20:55 +0900

    first commit

---
+++ 010-vpc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+    echo "deploy mode"
+    CHANGESET_OPTION=""
+fi
+
+# 指定パラメータ
+SYSTEM_NAME=test
+TEMPLATE=vpc
+
+# テンプレート実行用パラメータ
+CFN_STACK_NAME=${SYSTEM_NAME}-${TEMPLATE}
+CFN_TEMPLATE=template/${TEMPLATE}.yml
+
+aws cloudformation deploy --stack-name ${CFN_STACK_NAME} --template-file ${CFN_TEMPLATE} ${CHANGESET_OPTION} \
+  --parameter-overrides \
+  SystemName=${SYSTEM_NAME}
+
+exit
